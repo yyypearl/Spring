23f8e4cb49ab91fe3580c861c629bbfd32994783
yyypearl <hor629@naver.com>
2024-12-20 17:22:52 +0900

    [Refactor] AppConfig 리팩토링 (Session4)

--- core/src/main/java/spring/core/AppConfig.java
+++ core/src/main/java/spring/core/AppConfig.java
@@ -1,5 +1,6 @@
 package spring.core;
 
+import spring.core.discount.DisCountPolicy;
 import spring.core.discount.FixDiscountPolicy;
 import spring.core.member.MemberService;
 import spring.core.member.MemberServiceImpl;
@@ -10,11 +11,19 @@ import spring.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy()) {
+        return new OrderServiceImpl(memberRepository(), disCountPolicy()) {
         };
+    }
+
+    public DisCountPolicy disCountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
